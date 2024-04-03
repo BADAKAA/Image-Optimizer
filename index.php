d8e34148ca1ff470f058cb0ed3798760c893a8b7
BADAKAA <github.posh566@passinbox.com>
2024-04-03 03:21:43 +0200

    implement drag and drop

--- index.php
+++ index.php
@@ -1,8 +1,9 @@
-<?php 
-  require_once './config.php';
+<?php
+require_once './config.php';
 ?>
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -12,18 +13,22 @@
   <link rel="shortcut icon" href="./assets/img/picture.png" type="image/x-icon">
   <!-- https://codepen.io/luizomf/pen/YzaKqEB -->
 </head>
+
 <body>
   <h1>Image Optimizer</h1>
   <form action="./compress" method="POST" enctype="multipart/form-data">
-    <label for="imgToCompress" class="preview-label">
-      <img src="" alt="Preview" aria-hidden="true" class="preview-img">
-    </label>
-    <label class="picture" for="imgToCompress">
-      <img class="icon" src="./assets/img/picture.png" alt="" aria-hidden="true">
-      <p>Drop images here or <em>browse</em></p>
-      <small>Supported formats: <?= implode(', ',ALLOWED_FORMATS) ?></small>
-    </label>
-    <input type="file" name="imgToCompress" id="imgToCompress">
+    <div class="dropzone">
+      <label for="imgToCompress" class="preview-label">
+        <img src="" alt="Preview" aria-hidden="true" class="preview-img">
+      </label>
+      <label class="picture dropzone" for="imgToCompress">
+        <img class="icon" src="./assets/img/picture.png" alt="" aria-hidden="true">
+        <p>Drop images here or <em>browse</em></p>
+        <small>Supported formats: <?= implode(', ', ALLOWED_FORMATS) ?></small>
+      </label>
+      <input type="file" name="imgToCompress" id="imgToCompress">
+    </div>
+
     <input type="hidden" name="referrer" value="<?= $_SERVER['REQUEST_URI'] ?>">
     <div class="user-inputs">
       <div class="dimensions">
@@ -38,8 +43,8 @@
         <div>
           <span class="description">Format</span>
           <select name="format" id="">
-            <?php foreach (OUTPUT_FORMATS as $format) {?>
-              <option value="<?= $format ?>" <?= ($format===DEFAULT_FORMAT) ? 'selected':''?>><?= $format?></option>
+            <?php foreach (OUTPUT_FORMATS as $format) { ?>
+              <option value="<?= $format ?>" <?= ($format === DEFAULT_FORMAT) ? 'selected' : '' ?>><?= $format ?></option>
             <?php } ?>
 
           </select>
@@ -49,8 +54,9 @@
     </div>
   </form>
   <footer>
-    <a  href="https://www.flaticon.com/free-icons/picture" title="picture icons">Picture icon created by Freepik - Flaticon</a>
+    <a href="https://www.flaticon.com/free-icons/picture" title="picture icons">Picture icon created by Freepik - Flaticon</a>
     <a href="https://agbere.com/impressum">Imprint</a>
   </footer>
 </body>
+
 </html>
--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 const inputFile = document.querySelector("#imgToCompress");
 const previewImage = document.querySelector(".preview-img");
 
-inputFile.addEventListener("change", function (e) {
+function readFile(e) {
   const inputTarget = e.target;
   const file = inputTarget.files[0];
 
@@ -13,4 +13,23 @@ inputFile.addEventListener("change", function (e) {
     document.body.classList.toggle("has-preview",true);
   });
   reader.readAsDataURL(file);
+}
+
+
+inputFile.addEventListener("change", readFile);
+
+// https://codepen.io/tusharbandal/pen/YWadbB
+inputFile.addEventListener('dragover', function(e) {
+  e.preventDefault();
+  e.stopPropagation();
+  document.body.classList.toggle('dragover',true);
+});
+inputFile.addEventListener('dragleave', function(e) {
+  e.preventDefault();
+  e.stopPropagation();
+  document.body.classList.toggle('dragover',false);
+});
+
+inputFile.addEventListener('drop', function(e) {
+  document.body.classList.toggle('dragover',false);
 });
